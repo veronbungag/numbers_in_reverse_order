a4a22b0caf1af74da4d9fd6508579c89fcc6906c
veronbungag <rveron.bungag@gmail.com>
2024-01-24 21:49:13 +0800

    added reverse

--- numbers_in_reverse_order.py
+++ numbers_in_reverse_order.py
@@ -1,6 +1,7 @@
 #define a function for reverse digits
 def reverse_digits(number):
-#add reverse as 0
+    #add reverse as 0
+    reverse = 0
 #while loop
 #add return to reverse
 #add the numbers then print
