2bf306935be06cad1b9f151db6b51e0d0ddf5182
veronbungag <rveron.bungag@gmail.com>
2024-01-24 21:54:50 +0800

    while loop added

--- numbers_in_reverse_order.py
+++ numbers_in_reverse_order.py
@@ -2,6 +2,9 @@
 def reverse_digits(number):
     #add reverse as 0
     reverse = 0
-#while loop
+    #while loop
+    while number > 0:
+        reverse = reverse * 10 + (number % 10)
+        number = number // 10
 #add return to reverse
 #add the numbers then print
